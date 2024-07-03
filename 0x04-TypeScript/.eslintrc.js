78b3e99fd65b96935329bb91c76144ec7446ad36
Waliidbn0 <waliidbenhammii@gmail.com>
2024-07-03 21:36:42 +0000

    Typescript

---
+++ 0x04-TypeScript/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    parser: '@typescripte-eslint/parser',
+    extends: [
+        'pluging:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    ],
+    parserOprion: {
+        ecmaVersion: 2018,
+        sourceType: 'module'
+    },
+    rules: {
+    },
+};
